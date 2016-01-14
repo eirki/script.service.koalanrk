980ff30cd9d1f2c41370865cf3982a6bc433f4fa
eirki <eirik.b.stavest@gmail.com>
2016-01-14 15:06:14 +0100

    remove pyhook, pyuserinput, win32

--- lib/win32/pythoncom.py
+++
@@ -1,7 +0,0 @@
-# Magic utility that "redirects" to pythoncomxx.dll
-# import pywintypes
-# pywintypes.__import_pywin32_system_module__("pythoncom", globals())
-import _win32sysloader
-import imp
-_win32sysloader.LoadModule("pythoncom27.dll")
-imp.load_dynamic("pythoncom", r"C:\Programmer\Kodi\portable_data\addons\script.service.koalanrk\lib\win32\pywin32_system32\pythoncom27.dll")
