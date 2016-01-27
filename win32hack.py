f234f89212828919e239b06df6d4390b8f016f2e
eirki <eirik.b.stavest@gmail.com>
2016-01-27 22:31:03 +0100

    changed win32hack from service to daemon

--- win32hack.py
+++ win32hack.py
@@ -2,22 +2,32 @@ import os
 import sys
 import xbmc
 import xbmcgui
+from multiprocessing.dummy import Process as Thread
 
 from lib.utils import (os_join, const, log)
-log.info("win32 importer hack opening")
+log.info("win32 importer hack launching")
+
 os.environ["PATH"] += ";%s" % os_join(const.addonpath, "lib", "win32", "pywin32_system32")
 sys.path.extend([os_join(const.addonpath, "lib", "win32"),
                  os_join(const.addonpath, "lib", "win32", "win32"),
                  os_join(const.addonpath, "lib", "win32", "win32", "lib"),
                  os_join(const.addonpath, "lib", "win32", "pypiwin32-219.data", "scripts"),
                  os_join(const.addonpath, "lib", "win32", "Pythonwin")])
-from win32com.client import Dispatch
-import pywintypes
-import win32gui
 
 
-if __name__ == "__main__" and not xbmcgui.Window(10000).getProperty("win32 importer hack running") == "true":
-    log.info("win32 importer hack running in background")
-    xbmcgui.Window(10000).setProperty("win32 importer hack running", "false")
+def daemon_service():
+    xbmcgui.Window(10000).setProperty("win32 importer hack running", "true")
+    log.info("win32 importer hack service running in background")
+    from win32com.client import Dispatch
+    import pywintypes
+    import win32gui
     xbmc.Monitor().waitForAbort()
-log.info("win32 importer exiting")
+
+
+def run():
+    if xbmcgui.Window(10000).getProperty("win32 importer hack running") == "true":
+        log.info("win32 importer daemon already running, exiting")
+        return
+    daemon = Thread(target=daemon_service)
+    daemon.daemon = True
+    daemon.start()
